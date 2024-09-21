77e3ab136b23bb815228506154173ac67a9f808e
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-09-20 21:22:50 -0500

    Create connectSQL.php

---
+++ connectSQL.php
@@ -0,0 +1,55 @@
+//MySQLi (object-oriented)
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+//close connection
+$conn->close();
+?>
+
+//MySQLi Procedural
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+//close connection
+mysqli_close($conn);
+?>
+
+//PDO
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully"; 
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+//close connection
+$conn = null;
+?>
