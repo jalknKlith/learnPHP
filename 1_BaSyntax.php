f7967138be18a2d2169dfde20265c3a52158a1bd
jalknKlith <alejalkndro@gmail.com>
2024-11-03 10:04:27 -0500

    Basic Syntax

--- 1_BaSyntax.php
+++ 1_BaSyntax.php
@@ -10,17 +10,14 @@ LearnPHP
 $variable = "PHP";
 
 echo <<<HTML
-<p>La extensión por defecto de los archivos $variable es '.php'.</p>
-
-<p>Un archivo $variable normalmente contiene etiquetas HTML y algunos scripts de $variable.</p>
-
-<p>Una declaración en $variable siempre termina con punto y coma (;).</p>
-
-<p>En $variable las palabras clave (if, else, while, echo), clases, funciones pueden ir en mayúscula o minúscula (ej. ECHO, echo, EcHo).</p>
-
-<p>ECHO es lo mismo que echo.</p>
-
-<p>Pero las variables sí son sensibles a mayúsculas y minúsculas.</p>
+<p>La extensión por defecto de los archivos $variable es '.php'.
+<br><br>    
+Un archivo $variable normalmente contiene etiquetas HTML y algunos scripts de $variable.
+<br><br>
+Una declaración en $variable siempre termina con punto y coma (;).
+<br><br>
+En $variable las palabras clave (if, else, while, echo), clases, funciones pueden ir en mayúscula o minúscula (ej. ECHO, echo, EcHo). ECHO es lo mismo que echo. Pero las variables sí son sensibles a mayúsculas y minúsculas.</p>
+<br><br>
 HTML;
 
 ?>
