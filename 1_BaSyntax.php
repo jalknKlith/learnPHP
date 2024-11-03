25adef8534eb2226e0a92def6d536046d6259dc6
jalknKlith <alejalkndro@gmail.com>
2024-11-03 12:40:55 -0500

    Basic Syntax

--- 1_BaSyntax.php
+++ 1_BaSyntax.php
@@ -5,26 +5,45 @@
 <h1>
 LearnPHP
 </h1>
+<h3>
+Sintaxis básica
+</h3>
 
 <?php
-//Este es un comentario de una linea
-#Este tambien es un comentario de una linea
+// Este es un comentario de una linea
+# Este tambien es un comentario de una linea
 /*
 Este es un comentario de multilinea
 */
+$A = 8 /*Este es un comentario dentro del codigo*/ + 5;
 $variable = "PHP";
 
 echo <<<HTML
 <p>La extensión por defecto de los archivos $variable es '.php'.
 <br><br>    
-Un archivo $variable normalmente contiene etiquetas HTML y algunos scripts de $variable.
+Un archivo $variable normalmente contiene etiquetas HTML y algunos scripts en $variable.
 <br><br>
 Una declaración en $variable siempre termina con punto y coma (;).
 <br><br>
-En $variable las palabras clave (if, else, while, echo), clases, funciones pueden ir en mayúscula o minúscula (ej. ECHO, echo, EcHo). ECHO es lo mismo que echo. Pero las variables sí son sensibles a mayúsculas y minúsculas.</p>
+En $variable las palabras clave (if, else, while, echo), clases, funciones pueden ir en mayúscula o minúscula (ej. ECHO, echo, EcHo). ECHO es lo mismo que echo. Pero las variables sí son sensibles a mayúsculas y minúsculas.
 <br><br>
+Una variable comienza con el signo $ seguido por el nombre de la variable.
+<br><br> 
+Los comentaios $variable van con los signo // o /* Ej.
+<ul>
+  <li>// Este es un comentario de una linea</li>
+  <li># Este tambien es un comentario de una linea</li>
+  <li>/*Este es un comentario de multilinea*/</li>
+</ul>
+En $variable hay dos maneras para obtener resultados con echo y print. Ej.
+<ul>
+  <li>echo 'texto'</li>
+  <li>echo "texto"</li>
+  <li>print "texto"</li>
+  <li>print ("texto")</li>
+</ul>
+</p>
 HTML;
-
 ?>
 
 </body>
---
+++ 2_dataTypes.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>LearnPHP</h1>
+<h3>Tipos de datos</h3>
+
+<?php
+$x = "datos";
+$P = 'PHP';
+$y = array(2,3,4);
+$z = false;
+//var_dump($x);
+class Car{
+  public $color;
+  public $model;
+}
+//casting una vaiable, cambiar el tipo de dato de una variable
+$a = 5;
+$a = (string)$x;
+
+echo <<<HTML
+<p>Las variables pueden almacenar cualquier tipo de $x
+<br><br> 
+Las variables pueden ser declaradas en cualquier parte del script.
+<br><br>
+$P soporta los siguientes tipos de datos:
+<ul>
+  <li>string</li>
+  <li>Integer</li>
+  <li>float</li>
+  <li>boolean</li>
+  <li>array</li>
+  <li>object</li>
+  <li>null</li>
+  <li>resource</li>
+</ul>
+Para obtener el tipo de dato se usa la funcion var_dump()
+
+<h3>$P Strings</h3>
+Las cadenas pueden ser cualquier texto entre '' o "".
+<br><br>
+Un string es una secuencia de caracteres. Ej. "PHP es divertido"
+
+<h3>$P Integer</h3>
+Los enteros son numeros entre -2,147,483,648 and 2,147,483,647.
+<br><br>
+Las reglas de los enteros son:
+<ul>
+  <li>Deben tener por lo menos un digito</li>
+  <li>No deben tener un punto decimal</li>
+  <li>Pueden ser negativos o positivos</li>
+</ul>
+
+<h3>$P Float</h3>
+Los flotantes son numeros con punto decimal. Ej. 10.8
+<br><br>
+
+<h3>$P Boolean</h3>
+Los caracteres boleanos representa dos posibilidades. Verdadero o Falso.
+<br><br>
+
+<h3>$P Array</h3>
+Los vectores almacenan muchos valores en una sola variable.
+<br><br>
+
+<h3>$P Clase y Objectos</h3>
+Las clases y objetos son dos tipos de aspectos en programacion orienta a objetos.
+<br><br>
+Una clase es una plantilla para objetos, y un objeto es una instancia de una clase.
+<br><br>
+Cuando se crean los objetos, ellos heredan todas las propiedades y comportamientos de la clase, pero cada objeto tendrá valores diferentes para las propiedades.
+<br><br>
+<h3>$P Null</h3>
+Los datos nulos son variables sin un valor asignado, las variables sin un valor toma un valor único: NULL
+<br><br>
+</p>
+HTML;
+?>
+
+</body>
+</html>
