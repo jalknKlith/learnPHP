21a6e349ab26494e3015dc9b07f10a3607a136fa
jalknKlith <alejalkndro@gmail.com>
2024-11-03 17:40:23 -0500

    Basic Syntax

--- connectSQL.php
+++ connectSQL.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 //MySQLi (object-oriented)
 <?php
 $servername = "localhost";
@@ -53,3 +61,6 @@ try {
 //close connection
 $conn = null;
 ?>
+
+</body>
+</html>
--- createDBTable.php
+++ createDBTable.php
@@ -7,19 +7,28 @@
 </head>
 <body>
 <?php
-//PDO
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "mefort@1ece";
+$dbname = "testea";
 
 try {
-  $conn = new PDO("mysql:host=$servername", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "CREATE DATABASE testea";
+
+  // sql to create table
+  $sql = "CREATE TABLE users (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )";
+
   // use exec() because no results are returned
   $conn->exec($sql);
-  echo "Database created successfully<br>";
+  echo "Table Users created successfully";
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
