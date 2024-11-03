567f7c0ac9911f4c66aa67c3827baa9e59c467dc
jalknKlith <alejalkndro@gmail.com>
2024-11-03 09:31:40 -0500

    Basic syntaxis

---
+++ 1_BaSyntax.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+LearnPHP
+</h1>
+
+<?php
+echo "La extension por defecto de los archivo PHP es '.php.'<br><br> Un archivo PHP normalmente contiene etiquetas HTML, y algunos scripts de PHP.<br><br>Un declaracion en PHP siempre termina con punto y coma(;)<br><br>En PHP las palabras claves (if,else,while,echo), clases, fucnciones pueden ir en mayusculo o minuscula (ej. ECHO, echo, EcHo)";
+?>
+
+</body>
+</html>
